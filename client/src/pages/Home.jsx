8eafa29ebce8c89016faefee87e5b9d495b3ab2c
codewithrsk <rsk800001@gmail.com>
2026-06-18 22:36:28 +0530

    fix: update height of Home component div for full viewport display

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="bg-indigo-800 h-8 ">Home</div>
+      <div className="bg-indigo-800 h-full ">Home</div>
     </>
   );
 };
