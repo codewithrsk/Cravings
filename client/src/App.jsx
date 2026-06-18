650487fcb6467debfec924f572a136ffa8f2ef97
codewithrsk <rsk800001@gmail.com>
2026-06-18 22:44:48 +0530

    fix: add Terms and Conditions page and update routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import About from "./pages/About"
-import Contactus from "./pages/ContactUs"
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import About from "./pages/About";
+import Contactus from "./pages/ContactUs";
+import terms_and_conditions from "./pages/terms_and_conditions";
 const App = () => {
   return (
     <>
-    
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Routes>
           <Route path={"/"} element={<Home />} />
           <Route path={"/login"} element={<Login />} />
           <Route path={"/register"} element={<Register />} />
           <Route path={"/about"} element={<About />} />
           <Route path={"/contact-us"} element={<Contactus />} />
+
+          <Route
+            path={"/terms_and_conditions"}
+            element={<terms_and_conditions />}
+          />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/pages/Terms_and_conditions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const terms_and_conditions = () => {
+  return (
+   <>
+    <div>terms_and_conditions</div>
+    </>
+  )
+}
+
+export default terms_and_conditions
