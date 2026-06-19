6de0a9efbfba4a6f4c10231ce7a6710db87412fb
codewithrsk <rsk800001@gmail.com>
2026-06-19 21:58:34 +0530

    fix: correct component and export naming for Terms_and_conditions

--- client/src/pages/Terms_and_conditions.jsx
+++ client/src/pages/Terms_and_conditions.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const terms_and_conditions = () => {
+const Terms_and_conditions = () => {
   return (
    <>
-    <div>terms_and_conditions</div>
+    <div>Terms_and_conditions</div>
     </>
   )
 }
 
-export default terms_and_conditions
+export default Terms_and_conditions
