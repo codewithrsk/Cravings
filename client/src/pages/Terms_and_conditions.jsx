439b01ac9f980f7927b04fb7c77e10feec23a36b
codewithrsk <rsk800001@gmail.com>
2026-06-29 22:44:23 +0530

    style: standardize quotes and formatting in Terms_and_conditions component

--- client/src/pages/Terms_and_conditions.jsx
+++ client/src/pages/Terms_and_conditions.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Terms_and_conditions = () => {
   return (
-   <>
-    <div className='h-full w-full text-2xl'>Terms_and_conditions</div>
+    <>
+      <div className="h-full w-full text-2xl">Terms_and_conditions</div>
     </>
-  )
-}
+  );
+};
 
-export default Terms_and_conditions
+export default Terms_and_conditions;
