aac2eecc439292bcef686878df1992b7ca4b2bdc
codewithrsk <rsk800001@gmail.com>
2026-07-26 22:24:09 +0530

    fix: correct indentation in RestaurantOverview and ensure newline at end of file in RestaurantPhotos

--- client/src/components/restaurantDashboard/RestaurantOverview.jsx
+++ client/src/components/restaurantDashboard/RestaurantOverview.jsx
@@ -4,7 +4,7 @@ const RestaurantOverview = () => {
   return (
     <>
       <div className="bg-amber-50 w-fit m-7 text-2xl underline p-3">
-       Restauant Overview
+        Restauant Overview
       </div>
       <div className="w-52 p-3 bg-amber-50 m-10">Total Amount</div>
     </>
